431aab14b2dfd807aa358cdb5dc90ce20a0c54e0
TakeakiHirasawa <take38ugj441@aol.com>
2021-10-14 13:40:08 +0000

    laravel Mix

--- resources/views/admin/profile/create.blade.php
+++ resources/views/admin/profile/create.blade.php
@@ -1,14 +1,14 @@
 @extends('layouts.profile')
 
-{{-- admin.blade.phpの@yield('title')に'ニュースの新規作成'を埋め込む --}}
-@section('title', 'プロフィールの新規作成')
+{{-- profile.blade.phpの@yield('title')に'ニュースの新規作成'を埋め込む --}}
+@section('title', 'プロフィール')
         
-{{-- admin.blade.phpの@yield('content')に以下のタグを埋め込む --}}
+{{-- profile.blade.phpの@yield('content')に以下のタグを埋め込む --}}
 @section('content')
            <div class="container">
               <div class="row">
                   <div class="col-md-8 mx-auto">
-                      <h2>プロフィールの新規作成</h2>
+                      <h2>わたしのプロフィール</h2>
                   </div>
               </div>
            </div>
