ba99fb8f9abb4d839af6f13c144ae4c24e685aa3
TakeakiHirasawa <take38ugj441@aol.com>
2021-10-14 13:32:56 +0000

    Laravel Mix

--- resources/views/admin/profile/create.blade.php
+++ resources/views/admin/profile/create.blade.php
@@ -1,18 +1,17 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('layouts.profile')
 
-        <title>profile</title>
-    </head>
-    <body>
-        <h1>Myプロフィール</h1>
-        <ol>
-            <li>私の名前</li>
-            <li>私の居住地</li>
-            <li>私の電話番号</li>
-        </ol>
+{{-- admin.blade.phpの@yield('title')に'ニュースの新規作成'を埋め込む --}}
+@section('title', 'プロフィールの新規作成')
+        
+{{-- admin.blade.phpの@yield('content')に以下のタグを埋め込む --}}
+@section('content')
+           <div class="container">
+              <div class="row">
+                  <div class="col-md-8 mx-auto">
+                      <h2>プロフィールの新規作成</h2>
+                  </div>
+              </div>
+           </div>
+@endsection
     </body>
 </html>
