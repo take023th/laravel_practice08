1b355b5fa11eb85160d85b3c15bb4633e4a70142
TakeakiHirasawa <take38ugj441@aol.com>
2021-10-23 08:22:07 +0000

    editpage login

--- resources/views/admin/profile/edit.blade.php
+++ resources/views/admin/profile/edit.blade.php
@@ -1,18 +1,18 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('layouts.profile')
 
-        <title>editpage</title>
-    </head>
-    <body>
-        <h1>MyProfile editpage</h1>
-        <ul>
-            <li>氏名</li>
-            <li>住所</li>
-            <li>電話番号</li>
-        </ul>
-    </body>
-</html>
+@section('title','ProfileEdit')
+
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 mx-auto">
+                <h2>わたしの編集ページ</h2>
+                <ul>
+                    <li>氏名</li>
+                    <li>住所</li>
+                    <li>電話番号</li>
+                </ul>
+            </div>
+        </div>
+    </div>
+@endsection
